ac6a976e371969c14c63095518b2559e49ca4518
Harsh <harshkoradiya.glsica16@gmail.com>
2022-05-27 13:20:05 +0530

    Learna-about-git | Harsh Koradiya | Added boiler code

---
+++ src/main/java/learngit/learnaboutgit/LearnAboutGitApplication.java
@@ -0,0 +1,13 @@
+package learngit.learnaboutgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAboutGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAboutGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learngit/learnaboutgit/LearnAboutGitApplicationTests.java
@@ -0,0 +1,13 @@
+package learngit.learnaboutgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAboutGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
